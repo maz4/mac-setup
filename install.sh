d90e81a3e12c0764696aeb4fe90860f1f0f23a54
Marcin <zasadzkimarcin@gmail.com>
2022-06-21 23:04:36 +0100

    create ansible install yaml files

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "eval $(/opt/homebrew/bin/brew shellenv)" >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if ! command -v ansible &> /dev/null
+then
+    brew install ansible
+fi
+
+ansible-galaxy collection install community.general
+# ansible-playbook local.yaml -K --ask-vault-pass
+ansible-playbook local.yaml
