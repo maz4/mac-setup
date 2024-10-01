4c4dfef9080ae299d65a233d14ce792a0de7bfb2
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2024-10-02 07:01:21 +0900

    feat: add lvim config

---
+++ ansible/templates/options.lua
@@ -0,0 +1,24 @@
+-- User plugins config for lvim
+-- locaton lvim/lua/user/options.lua
+-- in you user/{user name}/config/lvim/config 
+-- import this file with require("user.options")
+lvim.plugins = {
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+      enabled = true,
+      auto_save = true,       -- Enables/disables auto saving session on exit
+      auto_restore = true,    -- Enables/disables auto restoring session on start
+      auto_create = true,     -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
+      use_git_branch = false, -- Include git branch name in session name
+
+    }
+  },
+}
