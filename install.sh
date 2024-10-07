f8b27b5359a9457ba21e673454bf114e47eb515c
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2024-10-07 22:48:42 +0900

    update install script to install packages

--- install.sh
+++ install.sh
@@ -15,12 +15,18 @@ fi
 ansible-galaxy collection install community.general
 ansible-playbook local.yaml -K --ask-vault-pass
 
+if command -v zsh &> /dev/null
+then
+  echo "Install LunarVim"
+  PIP_BREAK_SYSTEM_PACKAGES=1 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
+fi
+
+
 # Copy oh-my-zsh themes
 if command -v zsh &> /dev/null
 then
     echo "Copy oh-my-zsh theme"
     cp ansible/templates/cobalt2.zsh-theme ~/.oh-my-zsh/themes
-    cp ansible/templates/gnzh2.zsh-theme ~/.oh-my-zsh/themes
 fi
 
 # Copy Hamerspoon config
@@ -28,7 +34,7 @@ if [ -d "$HOME/.hammerspoon" ];
 then
     echo "Copy hammerspoon config to ~/.hammerspoon"
 
-    cp ansible/templates/init.lua ~/.hammerspoon/init.lua
+    cp ansible/templates/hammerspoonConfig.lua ~/.hammerspoon/init.lua
     
     echo "Hammerspoon config copied successfully"
 fi
@@ -40,3 +46,10 @@ then
     git config --global pretty.my format:'%C(yellow)%h %C(dim green)%ad %C(reset)| %C(cyan)%s%d %C(#667788)[%an]' --date=format:'%F %R'
     git config --global format.pretty my
 fi
+
+if command -v lvim &> /dev/null
+then
+  echo "Copy LunarVim config"
+  cp ansible/templates/nvimConfig.lua ~/.config/lvim/lua/user/options.lua
+fi
+ 
