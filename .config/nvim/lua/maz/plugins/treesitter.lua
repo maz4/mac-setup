db6d728f7a98c078976ec15357230b36b430fb0b
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2026-07-12 00:43:40 +0900

    fix treesitter plugin for 0.12.4 nvim version

--- .config/nvim/lua/maz/plugins/treesitter.lua
+++ .config/nvim/lua/maz/plugins/treesitter.lua
@@ -1,14 +1,30 @@
+-- Treesitter setup for nvim 0.12.4 and up
+-- This treesitter requiers tree-sitter-cli installed via brew
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	branch = "main",
+	lazy = false,
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+		local treesitter = require("nvim-treesitter")
 		treesitter.setup({
-			ensure_installed = { "lua", "javascript", "go", "markdown", "markdown_inline" },
-			sync_install = false,
-			auto_install = true,
-			hilight = { enable = true },
-			indent = { enable = true },
+			install_dir = vim.fn.stdpath("data") .. "/site",
 		})
+		treesitter
+			.install({
+				"lua",
+				"astro",
+				"typescript",
+				"javascript",
+				"go",
+				"markdown",
+				"markdown_inline",
+				"prisma",
+				"tsx",
+				"html",
+				"rust",
+				"kotlin",
+			})
+			:wait(300000) -- wait max. 5 minutes
 	end,
 }
